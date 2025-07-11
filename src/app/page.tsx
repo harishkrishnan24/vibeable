521dbda0b4f8f8ee16faaf49225b4b63c1d066cc
harishkrishnan24 <harishkrishnan1993@gmail.com>
2025-07-11 21:15:44 +0200

    feat: TRPC as data access layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import { Button } from "@/components/ui/button";
 
 const Page = () => {
-  return (
-    <div>
-      <Button variant="destructive">Click me</Button>
-    </div>
-  );
+  return <div>Hello</div>;
 };
 
 export default Page;
