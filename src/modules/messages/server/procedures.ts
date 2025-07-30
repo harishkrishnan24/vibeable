e0aeb23f163f1f1fe659ba03dd71953026ea03b5
harishkrishnan24 <harishkrishnan1993@gmail.com>
2025-07-30 21:46:57 +0200

    feat: add messaging functionality with message form and container components

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -4,15 +4,27 @@ import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { z } from "zod";
 
 export const messagesRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
-    const messages = await prisma.message.findMany({
-      orderBy: {
-        updatedAt: "desc",
-      },
-    });
+  getMany: baseProcedure
+    .input(
+      z.object({
+        projectId: z.string().min(1, { message: "Project ID is required" }),
+      })
+    )
+    .query(async ({ input }) => {
+      const messages = await prisma.message.findMany({
+        where: {
+          projectId: input.projectId, // Filter messages by project ID
+        },
+        orderBy: {
+          updatedAt: "asc",
+        },
+        include: {
+          fragment: true, // Include fragment data if needed
+        },
+      });
 
-    return messages;
-  }),
+      return messages;
+    }),
   create: baseProcedure
     .input(
       z.object({
